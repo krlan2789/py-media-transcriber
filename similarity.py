4f721aa82d34a04c65408641fc2945a774e6f826
Erlan Kurnia <33871129+krlan2789@users.noreply.github.com>
2024-10-04 15:35:19 +0700

    Update similarity.py

--- similarity.py
+++
@@ -1,7 +0,0 @@
-from sentence_transformers import SentenceTransformer, util
-
-model = SentenceTransformer('all-MiniLM-L6-v2')
-embeddings1 = model.encode("This is a sentence")
-embeddings2 = model.encode("This is another sentence")
-similarity = util.pytorch_cos_sim(embeddings1, embeddings2)
-print(similarity)
